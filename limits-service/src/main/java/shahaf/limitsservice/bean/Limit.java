d458f0bc0235c50cda84a366b7b18ac2065e9e83
ShahafErez <Shahaf30@gmail.com>
2023-12-04 17:57:53 +0200

    initial commit

---
+++ limits-service/src/main/java/shahaf/limitsservice/bean/Limit.java
@@ -0,0 +1,13 @@
+package shahaf.limitsservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Limit {
+    private int minimum;
+    private int maximum;
+}
