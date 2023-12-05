d8ece17b25d68ef07bd9a27dc82856d302e040a6
ShahafErez <Shahaf30@gmail.com>
2023-12-05 16:08:33 +0200

    added proxy to access exchange service

--- currency-conversion-service/src/main/java/shahaf/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/shahaf/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,19 +1,21 @@
 package shahaf.currencyconversionservice.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import shahaf.currencyconversionservice.dto.CurrencyConversion;
+import shahaf.currencyconversionservice.proxy.CurrencyExchangeProxy;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
+
+    @Autowired
+    CurrencyExchangeProxy currencyExchangeProxy;
 
     @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -21,21 +23,9 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal quantity
     ) {
-        // setting the values to be inserted into the url
-        HashMap<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
-                ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                        CurrencyConversion.class, uriVariables);
-
-        CurrencyConversion currencyConversion = responseEntity.getBody();
-        assert currencyConversion != null;
-        BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
-        return new CurrencyConversion(currencyConversion.getId(),
-                from, to, quantity,
-                conversionMultiple,
-                quantity.multiply(conversionMultiple));
+        CurrencyConversion currencyConversion = currencyExchangeProxy.getExchangeValue(from, to);
+        currencyConversion.setQuantity(quantity);
+        currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
+        return currencyConversion;
     }
 }
---
+++ currency-conversion-service/src/main/java/shahaf/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package shahaf.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import shahaf.currencyconversionservice.dto.CurrencyConversion;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversion getExchangeValue(@PathVariable String from, @PathVariable String to);
+}
