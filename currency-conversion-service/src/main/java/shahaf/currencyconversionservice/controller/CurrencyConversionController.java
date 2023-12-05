01f43657339e871bda1e02e1460bd5f58092538e
ShahafErez <Shahaf30@gmail.com>
2023-12-05 13:27:31 +0200

    minor refactoring to controller

--- currency-conversion-service/src/main/java/shahaf/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/shahaf/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package shahaf.currencyconversionservice.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import shahaf.currencyconversionservice.dto.CurrencyConversion;
@@ -11,33 +12,30 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
-    }
-
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity
     ) {
-
+        // setting the values to be inserted into the url
         HashMap<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from",from);
-        uriVariables.put("to",to);
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
 
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
                 ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
                         CurrencyConversion.class, uriVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
-
+        assert currencyConversion != null;
+        BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
         return new CurrencyConversion(currencyConversion.getId(),
                 from, to, quantity,
-                currencyConversion.getConversionMultiple(),
-                quantity.multiply(currencyConversion.getConversionMultiple()));
+                conversionMultiple,
+                quantity.multiply(conversionMultiple));
     }
 }
