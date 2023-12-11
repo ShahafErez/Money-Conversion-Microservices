e2740aa9762cac5501659da27fa084722415bc0e
ShahafErez <90973511+ShahafErez@users.noreply.github.com>
2023-12-11 21:24:07 +0200

    Error handling (#9)
    * Insert data on application setup
    * Added error handling at currency conversion service
    * Override currency exchange if already exists
    * deleted tests folders

--- currency-exchange-service/src/main/java/shahaf/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/shahaf/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,4 +1,4 @@
-package shahaf.currencyexchangeservice.models;
+package shahaf.currencyexchangeservice.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- currency-exchange-service/src/main/java/shahaf/currencyexchangeservice/reposetory/CurrencyExchangeRepository.java
+++ currency-exchange-service/src/main/java/shahaf/currencyexchangeservice/reposetory/CurrencyExchangeRepository.java
@@ -2,9 +2,11 @@ package shahaf.currencyexchangeservice.reposetory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import shahaf.currencyexchangeservice.models.CurrencyExchange;
+import shahaf.currencyexchangeservice.model.CurrencyExchange;
 
 @Repository
 public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    Boolean existsByFromAndTo(String from, String to);
     CurrencyExchange findByFromAndTo(String from, String to);
+    void deleteByFromAndTo(String from, String to);
 }
